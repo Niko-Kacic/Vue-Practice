a62bac8e02da2b4b14f7773a8875155a22c1bd91
NikoBoolean <niko.kacic@gmail.com>
2025-05-04 00:04:29 -0400

    new coments added

--- 01-TS-Bases/src/Base/07-imp-exp.ts
+++ 01-TS-Bases/src/Base/07-imp-exp.ts
@@ -1,17 +1,16 @@
-
+                //Al importar es bueno colocar el type, para la transpilación no lo considere
 import heroes, { type Owner } from "../data/heroes";
 //En el import de heroes, se trae la const por default de data
 //En cambio con {owners} se debe traer por destr ya que se exporta como array
 
 export const getHeroByID = (id:number)=>{
-    
    return heroes.find( hero => hero.id === id) ?? {};
 }
 
 
 //console.log(getHeroByID(100));
 
-export const getHeroByOwner = (owner:Owner)=>{
+export const getHeroByOwner = (owner:Owner)=>{//Se agrega tipado extricto de Owner
     return heroes.filter(hero => hero.owner === owner )??{};
 }
 
