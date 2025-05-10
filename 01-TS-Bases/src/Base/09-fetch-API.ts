00537d1ddf431eb2ca0186b0df0a8373652127f6
NikoBoolean <niko.kacic@gmail.com>
2025-05-10 19:30:17 -0400

    using fetch and transforming the readablestream

--- 01-TS-Bases/src/Base/09-fetch-API.ts
+++ 01-TS-Bases/src/Base/09-fetch-API.ts
@@ -1,3 +1,4 @@
+import type { GIFResponse } from "../interfaces/gif.response";
 
 
 const apiKey = '1gmFgtZpiABHDXM0RfJw8e9OkNdP1rB2';
@@ -5,10 +6,10 @@ const apiKey = '1gmFgtZpiABHDXM0RfJw8e9OkNdP1rB2';
 //fetch viene incluido
 fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
     .then((res)=>{
-        return res.json()
+        return res.json()//Se usa json() para transforma la respuesta que viene como ReadableStream y tomar lo que interesa
     })
-    .then((body)=>{
-        console.log({body})
+    .then((body:GIFResponse)=>{
+        console.log(body.data.images.downsized_medium.url);
     })
     .catch((err)=> {
         console.info(err)
