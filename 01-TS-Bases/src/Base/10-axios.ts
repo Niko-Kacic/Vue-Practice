979adcbd111ee4258024ec762d00905aefbd2b88
NikoBoolean <niko.kacic@gmail.com>
2025-05-10 20:22:29 -0400

    using asyn and await

--- 01-TS-Bases/src/Base/10-axios.ts
+++ 01-TS-Bases/src/Base/10-axios.ts
@@ -4,7 +4,7 @@ import { GIFResponse } from '../interfaces/gif.response';
 const apiKey = '1gmFgtZpiABHDXM0RfJw8e9OkNdP1rB2';
 
 
-const gifyApi = axios.create({ //Esto crea una instancia de axios con create() para trabajar directamente con la API de giphy 
+export const giphyApi = axios.create({ //Esto crea una instancia de axios con create() para trabajar directamente con la API de giphy 
    
     baseURL: 'https://api.giphy.com/v1/gifs',
     params: {
@@ -15,6 +15,6 @@ const gifyApi = axios.create({ //Esto crea una instancia de axios con create() p
 
 
 
-gifyApi.get<GIFResponse>('/random')
-    .then( res => console.log(res.data.data.images.downsized_large.url))
-    .catch( err => console.log( err))
+// giphyApi.get<GIFResponse>('/random')
+//     .then( res => console.log(res.data.data.images.downsized_large.url))
+//     .catch( err => console.log( err))
---
+++ 01-TS-Bases/src/Base/11-async-await.ts
@@ -0,0 +1,25 @@
+import { giphyApi } from "./10-axios";
+import type { GIFResponse } from '../interfaces/gif.response';
+
+export const getImage = async()=>{//async transforma la funciona, en una que regresa una promesa
+
+  try{
+
+      const resp = await giphyApi.get<GIFResponse>('/rasdndom')
+      return resp.data.data.images.downsized_large.url;
+    } catch(err){
+        throw 'URL no encontrada'
+    } 
+}
+
+// export const getImagePromise = ()=>{//Mismo resultado que con async
+//     return new Promise( resolve =>{
+//         resolve('https://url-de-sitio/image1.jpg')
+//     });
+// }
+
+
+getImage()
+    .then( url => console.log({url}))
+    .catch(err => console.log(err))
+//console.log(getImagePromise());
